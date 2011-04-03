30ec94892972c12ec3d717f7072c5ac5eba60f21
Kari Sullivan <kari.sullivan@gmail.com>
2011-04-03 16:46:48 -0500

    added examples for definitions

--- vocab.rb
+++ vocab.rb
@@ -1,7 +1,7 @@
 require 'rubygems'
 require 'wordnik-ruby'
 
-wordnik=Wordnik::Wordnik.new({:api_key=>'api key'})
+wordnik=Wordnik::Wordnik.new({:api_key=>'api_key'})
 
 puts "Give me a word:"
 word_input = gets.chomp
@@ -31,9 +31,37 @@ definition_number = gets.chomp
 
 definition_number = definition_number.to_i - 1
 
-puts word_input + "\n" + word_definitions[definition_number]
+open('vocab.txt', 'a') {|f|
+  f.puts "\n" + word_input + "\n" + "Definition: " + word_definitions[definition_number] 
+}
+
+puts "Would you like examples? (y/n)"
+want_examples = gets.chomp
+
+my_word_ex = my_word.examples
+
+i = 0
+
+word_examples = Array.new
+
+while i < my_word_ex.length
+   word_examples.push(my_word_ex[i].display)
+   i += 1
+end
+
+counter = 1
+
+for i in word_examples
+  puts counter.to_s + ": " + i
+  counter += 1
+end
+
+puts "What example do you want to save?"
+example_number = gets.chomp
+
+example_number = example_number.to_i - 1
 
 
 open('vocab.txt', 'a') {|f|
-  f.puts "\n" + word_input + "\n" + word_definitions[definition_number] 
+  f.puts "Example: " + word_examples[example_number] 
 }
